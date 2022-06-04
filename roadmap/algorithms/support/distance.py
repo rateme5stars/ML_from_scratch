cccf83f5ee2a501d0f4d99d151fcc3228ce1d518
rateme5stars <dzungardan@gmail.com>
2022-06-04 22:26:00 +0700

    add support

---
+++ roadmap/algorithms/support/distance.py
@@ -0,0 +1,17 @@
+import math
+
+class Distance:
+    # Distance between sample vs specific data point in dataset
+    @staticmethod
+    def euclidean(x1=None, x2=None):
+        distance = 0
+        for i in range(len(x1)):
+            distance += (x1[i] - x2[i])**2
+        return math.sqrt(distance)
+
+    @staticmethod
+    def manhattan(x1=None, x2=None):
+        distance = 0
+        for i in range(len(x1)):
+            distance += abs(x1[i] - x2[i])
+        return distance
